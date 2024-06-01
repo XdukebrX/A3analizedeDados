2261ae0068d878a2a416545ed290033c4e260051
andre <andretabajara70@gmail.com>
2024-06-01 12:20:36 -0300

    renamed class file from placeholder to actual name updated respective imports

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 
 from dotenv import load_dotenv, find_dotenv
 
-from src.domain.author import Author
+from src.domain.goalscorers import Author
 from src.utils.conection import connect_to_mysql
 
 
